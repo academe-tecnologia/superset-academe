e72e7de821c1b7e84491e1686e8a68795febd665
Elzevir Sá <51340237+codaisa@users.noreply.github.com>
2025-12-01 19:19:46 -0300

    Update superset_config.py

--- config/superset_config.py
+++ config/superset_config.py
@@ -7,14 +7,26 @@ FEATURE_FLAGS = {
 }
 
 TALISMAN_ENABLED = False
-PUBLIC_ROLE_LIKE = "Gamma" 
-
-ENABLE_PROXY_FIX = True
-
-HTTP_HEADERS = {
-    "X-Frame-Options": "ALLOWALL",
-}
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE")
+
+SESSION_COOKIE_SAMESITE = "None"
+ENABLE_PROXY_FIX = True
+PUBLIC_ROLE_LIKE_GAMMA = True
+SESSION_COOKIE_SECURE = True
+CSRF_ENABLED = False
+WTF_CSRF_ENABLED = False
+GUEST_ROLE_NAME = "Gamma"
+GUEST_TOKEN_JWT_SECRET = "test-guest-secret-change-me"
+GUEST_TOKEN_JWT_ALGO = "HS256"
+GUEST_TOKEN_HEADER_NAME = "X-GuestToken"
+GUEST_TOKEN_JWT_EXP_SECONDS = 300
+
+CORS_OPTIONS = {
+    'supports_credentials': True,
+    'allow_headers': ['*'],
+    'resources': ['*'],
+    'origins': ['*']
+}
